03fd129be2443ec522c6c18db78995cffca79e09
Calli Gross <ggthemeassist@icloud.com>
2016-04-23 08:41:22 +0000

    version 0.1.4

--- R/constants.R
+++ R/constants.R
@@ -1,40 +1,87 @@
 # Choices to pick from
-colours.available <- c('None' = NA, colors())
+colours.available <- c('None' = NA, colors()[!grepl('grey', colors())]) # We don't want redundant grays
 text.faces <- c('plain', 'italic', 'bold', 'bold.italic')
-text.families <- names(pdfFonts())
+
+text.families <- if (is.element('extrafont', installed.packages()[, 1])) {
+  c(c('sans', 'serif', 'mono'), extrafont::fonttable()$FamilyName)
+} else {
+  names(pdfFonts())
+}
+
 legend.positions <- c('none', 'left', 'right', 'top', 'bottom', 'XY')
 legend.directions <- c('horizontal', 'vertical')
 linetypes <- c('blank', 'solid', 'dashed', 'dotted', 'dotdash', 'longdash', 'twodash')
 
 # Configurations
 input.width <- '50%'
+input.width2 <- '90%'
 line.height <- '70px'
 heading.height <- '30px'
 
 
 # default values
 default <- list(
-  axis.text = list(
+  plot.subtitle = structure(list(
+    family = 'sans',
+    size = 9,
+    face = 'plain',
+    colour = 'gray30',
+    hjust = 0,
+    vjust = 0.5,
+    angle = 0,
+    lineheight = 1.1
+  ), class = 'element_text'),
+  plot.caption = structure(list(
+    family = 'sans',
+    size = 9,
+    face = 'plain',
+    colour = 'gray30',
+    hjust = 1,
+    vjust = 0.5,
+    angle = 0,
+    lineheight = 1.1
+  ), class = 'element_text'),
+  axis.text = structure(list(
     family = 'sans',
     size = 10,
     face = 'plain',
-    colour = 'grey30',
+    colour = 'gray30',
     hjust = 0.5,
     vjust = 0.5,
     angle = 0,
     lineheight = 1.1
-    ),
-  axis.line = list(
+    ), class = 'element_text'),
+  axis.text.x = structure(list(
+    family = 'sans',
+    size = 10,
+    face = 'plain',
+    colour = 'gray30',
+    hjust = 0.5,
+    vjust = 1,
+    angle = 0,
+    lineheight = 1.1
+  ), class = 'element_text'),
+  axis.text.y = structure(list(
+    family = 'sans',
+    size = 10,
+    face = 'plain',
+    colour = 'gray30',
+    hjust = 1,
+    vjust = 0.5,
+    angle = 0,
+    lineheight = 1.1
+  ),  class = 'element_text'),
+  axis.line = structure(list(
     colour = 'black',
     size = 1,
     linetype = 'blank'
-    ),
-  axis.ticks = list(
-    colour = 'grey20',
+    ), class = 'element_line'),
+  axis.ticks = structure(list(
+    colour = 'gray20',
     size = 0.5,
     linetype = 'solid'
-  ),
-  axis.title = list(
+  ), class = 'element_line'),
+  axis.title = structure(list(
     family = 'sans',
     size = 11,
     face = 'plain',
@@ -43,63 +90,63 @@ default <- list(
     vjust = 0.5,
     angle = 0,
     lineheight = 1.1
-  ),
-  plot.title = list(
+  ), class = 'element_text'),
+  plot.title = structure(list(
     family = 'sans',
     size = 13,
     face = 'plain',
     colour = 'black',
-    hjust = 0.5,
+    hjust = if (any(names(formals(ggtitle)) == 'subtitle')) {0} else {0.5},
     vjust = 0.5,
     angle = 0,
     lineheight = 1.1
-  ),
-  panel.background = list(
-    fill = 'grey92',
+  ), class = 'element_text'),
+  panel.background = structure(list(
+    fill = 'gray92',
     colour = 'NA',
     size = 0.5,
     linetype = 'blank'
-  ),
-  plot.background = list(
+  ), class = 'element_rect'),
+  plot.background = structure(list(
     fill = 'NA',
     colour = 'white',
     size = 0.5,
     linetype = 'blank'
-  ),
-  panel.grid.major = list(
-    colour = 'grey100',
+  ), class = 'element_rect'),
+  panel.grid.major = structure(list(
+    colour = 'gray100',
     size = 0.5,
     linetype = 'solid'
-  ),
-  panel.grid.minor = list(
-    colour = 'grey100',
+  ), class = 'element_line'),
+  panel.grid.minor = structure(list(
+    colour = 'gray100',
     size = 0.5,
     linetype = 'solid'
-  ),
-  legend.text = list(
+  ), class = 'element_line'),
+  legend.text = structure(list(
     size = 10,
     face = 'plain',
     family = 'sans',
     colour = 'black'
-  ),
-  legend.title = list(
+  ), class = 'element_text'),
+  legend.title = structure(list(
     size = 10,
     face = 'plain',
     family = 'sans',
     colour = 'black'
-  ),
-  legend.background = list(
-    fill = 'grey100',
+  ), class = 'element_text'),
+  legend.background = structure(list(
+    fill = 'gray100',
     colour = 'NA',
     size = 0.5,
     linetype = 'blank'
-  ),
-  legend.key = list(
-    fill = 'grey95',
+  ), class = 'element_rect'),
+  legend.key = structure(list(
+    fill = 'gray95',
     colour = 'NA',
     size = 0.5,
     linetype = 'blank'
-  ),
+  ), class = 'element_rect'),
   legend.position = 'right',
   legend.position.x = 0.5,
   legend.position.y = 0.5,
@@ -109,11 +156,27 @@ default <- list(
     x = '',
     y = '',
     colour = '',
-    fill = ''
+    fill = '',
+    size = '',
+    linetype = '',
+    shape = '',
+    alpha = '',
+    subtitle = '',
+    caption = ''
   )
 )
 
 AvailableElements <- list(
+  plot.subtitle = list(
+    name = 'plot.subtitle',
+    type = 'element_text',
+    enabled = TRUE
+  ),
+  plot.caption = list(
+    name = 'plot.caption',
+    type = 'element_text',
+    enabled = TRUE
+  ),
   axis.line = list(
     name = 'axis.line',
     type = 'element_line',
@@ -141,6 +204,16 @@ AvailableElements <- list(
   ),
   axis.text = list(
     name = 'axis.text',
+    type = 'element_text',
+    enabled = TRUE
+  ),
+  axis.text.x = list(
+    name = 'axis.text.x',
+    type = 'element_text',
+    enabled = TRUE
+  ),
+  axis.text.y = list(
+    name = 'axis.text.y',
     type = 'element_text',
     enabled = TRUE
   ),
--- R/updateDefaults.R
+++ R/updateDefaults.R
@@ -1,5 +1,27 @@
-updateDefaults <- function(gg, defaults) {
-  LegendPosition <- NULL
+updateDefaults <- function(gg, defaults, linetypes = linetypes) {
+
+  # Set default values for all elements
+  for (i in c('rect', 'line', 'text')) {
+    #substitute numeric linetypes to named, otherwise input widgets can't handle them
+    if (i %in% c('rect', 'line') && !is.null(gg$theme[[i]]['linetype']) && is.numeric(gg$theme[[i]][['linetype']])) {
+      gg$theme[[i]]['linetype'] <- linetypes[(gg$theme[[i]][['linetype']] + 1)]
+    }
+
+    # find elements in defaults with the class of i
+    ThemeClass <- class(gg$theme[[i]])[1]
+    DefaultClasses <- sapply(defaults, class)
+    DefaultClasses <- DefaultClasses[DefaultClasses == ThemeClass]
+    DefaultClasses <- names(DefaultClasses)
+    if (length(DefaultClasses) > 0) {
+      for (j in DefaultClasses) {
+        gg$theme[[i]][which(gg$theme[[i]][names(default[[j]])] == '')] <- NULL
+        default[[j]][names(gg$theme[[i]])] <- lapply(gg$theme[[i]][names(gg$theme[[i]])], unname)
+      }
+    }
+  }
+
+
+    LegendPosition <- NULL
   if (length(gg$theme$legend.position) > 1) {
     LegendPosition <- gg$theme$legend.position
     gg$theme$legend.position <- 'XY'
@@ -11,7 +33,7 @@ updateDefaults <- function(gg, defaults) {
 
   # get common names
   NamesThemes <- names(ThemeOptions)
-  NamesDefaults <- names(unlist(default))
+  NamesDefaults <- names(unlist(defaults))
   CommonNames <- NamesThemes[NamesThemes %in% NamesDefaults]
 
   CommonNames <- CommonNames[!CommonNames %in% c('legend.position.x', 'legend.position.y')]
@@ -23,16 +45,28 @@ updateDefaults <- function(gg, defaults) {
     element <- gsub(pattern = '^.*\\.', '', i)
     if (i == 'legend.position' || i == 'legend.direction') {
       if (!is.null(LegendPosition) && i == 'legend.position') {
-        default['legend.position'] <- 'XY'
-        default['legend.position.x'] <- LegendPosition[1]
-        default['legend.position.y'] <- LegendPosition[2]
+        defaults['legend.position'] <- 'XY'
+        defaults['legend.position.x'] <- LegendPosition[1]
+        defaults['legend.position.y'] <- LegendPosition[2]
       } else {
-        default[i] <- gg$theme[i]
+        defaults[i] <- gg$theme[i]
       }
     }
     else {
-      default[[anchor]][[element]] <- gg[['theme']][[anchor]][[element]]
+      # Relative size needs to be converted to absolute sizes
+      if (class(gg[['theme']][[anchor]][[element]]) == 'rel') {
+        defaults[[anchor]][[element]] <- as.numeric(gg[['theme']][[anchor]][[element]]) * gg$theme$text$size
+      } else {
+        if (element == 'linetype' && is.numeric(gg[['theme']][[anchor]][[element]])) {
+          defaults[[anchor]][[element]] <- linetypes[gg[['theme']][[anchor]][[element]] + 1]
+        } else {
+          defaults[[anchor]][[element]] <- gg[['theme']][[anchor]][[element]]
+        }
+      }
     }
   }
-  return(default)
+
+  # substitute grey with gray
+  defaults <- rapply(defaults, function(x) { gsub('grey', 'gray', x) }, how = 'list')
+  return(defaults)
 }
